4e4770293145c8f212d35cee8a7aefe8ddaf23a2
Miron Banks <madapegarage@gmail.com>
2020-03-13 02:15:56 +0100

    styled component

---
+++ src/pages/news.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const NewsPage = () => (
+    <Layout>
+        <h1>News Page</h1>
+        <p>welcome to my blog</p>
+    </Layout>
+)
+
+export default NewsPage
